ec194f001bac249b0132794d7eab24b153d787c0
qin2500 <52422134+qin2500@users.noreply.github.com>
2024-08-18 12:12:52 -0400

    fixed bug where guys get stuck on platforms, but new problem where they start lagging behind.

--- Assets/LilGuyFollowAI.cs
+++ Assets/LilGuyFollowAI.cs
@@ -7,9 +7,10 @@ public class LilGuyFollowAI : MonoBehaviour
     private LilGuyFollowAI follower;
     public Rigidbody2D rb;
     public float speed = 5f;
-    public float followDistance = 0.1f; // Adjust this value to define how close the AI should be before dequeuing
+    public float followDistance = 0.01f; // Adjust this value to define how close the AI should be before dequeuing
 
     private Vector2 curTarget;
+    private float timeAC;
 
     private void Start()
     {
@@ -18,12 +19,14 @@ public class LilGuyFollowAI : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (curTarget == Vector2.zero || Vector2.Distance(transform.position, curTarget) <= followDistance)
+        timeAC += Time.deltaTime;
+        if (curTarget == Vector2.zero || Vector2.Distance(transform.position, curTarget) <= followDistance || timeAC > 0.5f)
         {
             if (target.getPositionHistory().Count > 0)
             {
                 curTarget = target.getPositionHistory().Dequeue();
             }
+            timeAC = 0f;
         }
 
         Vector2 direction = curTarget - rb.position;
