dde562fbb2138578cbc0cee5b9d3d0a70b0a79cc
qin2500 <52422134+qin2500@users.noreply.github.com>
2024-08-20 03:30:01 -0400

    Penis

--- Assets/Script/Paralax Controller/ParallaxController.cs
+++ Assets/Script/Paralax Controller/ParallaxController.cs
@@ -5,32 +5,45 @@ using UnityEngine.UIElements;
 
 public class ParallaxController : MonoBehaviour
 {
-    private float startPos, length;
+    private float startPosx, startPosy, length, height;
     public GameObject cam;
-    public float parallaxEffect;
+    public Vector2 parallaxEffect;
     // Start is called before the first frame update
     void Start()
     {
-        startPos = transform.position.x;
+        startPosx = transform.position.x;
+        startPosy = transform.position.y;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
+        height = GetComponent<SpriteRenderer>().bounds.size.y;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float distance = cam.transform.position.x * parallaxEffect;
+        Vector2 distance = (Vector2)cam.transform.position * parallaxEffect;
 
-        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+        transform.position = new Vector3(startPosx + distance.x, startPosy + distance.y, transform.position.z);
 
-        float movement = cam.transform.position.x * (1 - parallaxEffect);
+        float movementx = cam.transform.position.x * (1 - parallaxEffect.x);
 
-        if(movement > startPos + length)
+        if(movementx > startPosx + length)
         {
-            startPos += length;
+            startPosx += length;
         }
-        else if (movement < startPos - length)
+        else if (movementx < startPosx - length)
         {
-            startPos -= length;
+            startPosx -= length;
+        }
+
+        float movementy = cam.transform.position.y * (1 - parallaxEffect.y);
+
+        if (movementy > startPosy + length)
+        {
+            startPosy += length;
+        }
+        else if (movementy < startPosy - length)
+        {
+            startPosy -= length;
         }
     }
 }
